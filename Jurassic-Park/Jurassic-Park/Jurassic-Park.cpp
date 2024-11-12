64c6c6e67d72ac0313cc5f6d22d2dc230ed3a80c
khemakhemsofian <sofian.khemakhem@gmail.com>
2024-11-12 14:30:59 +0100

    V1

--- Jurassic-Park/Jurassic-Park/Jurassic-Park.cpp
+++ Jurassic-Park/Jurassic-Park/Jurassic-Park.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+class Personnage {
+private:
+
+    string regime;
+    int faim;
+    int hp;
+    bool estChasse;
+    bool chasse;
+public:
+    dyno(string regime, int hp, int faim :regime(regime), faim(faim), hp(hp) {}
+
+    void baissefaim() {
+        while (0 < faim) {
+            faim = faim - 5;
+        }
+    }
+};
+
+
 int main()
 {
 
