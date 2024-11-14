020b9cf3a0be5ef24fbce524e9c7f13224a98ee4
Githubcours <lilianlatrille2+githublisaa@gmail.com>
2024-11-14 14:35:59 +0100

    A

--- Jurassic-Park/Jurassic-Park/Animal.h
+++ Jurassic-Park/Jurassic-Park/Animal.h
@@ -7,7 +7,7 @@ float gennbint(int nb1, int nb2) {
     return floor(nb1 + ((rand() / (float)RAND_MAX) * nb2));
 }
 
-class bet {
+class Animal {
 private:
     std::string nom;
     int grangou;
@@ -19,11 +19,7 @@ public:
     int x;
     int y;
 
-<<<<<<< Updated upstream
-    bet(int hp, int faim, int predation) : grangou(faim), lavi(hp), predasyon(predation), x(gennbint(0, 20)), y(gennbint(0, 20)) {}
-=======
     Animal(std::string name, int hp, int faim) : nom(name), grangou(faim), lavi(hp), x(gennbint(0, 20)), y(gennbint(0, 20)) {}
->>>>>>> Stashed changes
 
     void test() { std::cout << nom << ", faim: " << grangou << ", vie: " << lavi << ", x: " << x << " y: " << y; }
 };
