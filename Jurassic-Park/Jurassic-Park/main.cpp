fcb2e384aaed274c5deb74253705ca8fec9449aa
Githubcours <lilianlatrille2+githublisaa@gmail.com>
2024-11-14 14:36:17 +0100

    A

--- Jurassic-Park/Jurassic-Park/main.cpp
+++ Jurassic-Park/Jurassic-Park/main.cpp
@@ -4,14 +4,8 @@
 
 int main()
 {
-<<<<<<< Updated upstream
-	std::cout << "Entrer la taille du tableau:\n> ";
-	std::cin >> tablo_gwosè;
-	std::cout << "ZUB";
-=======
 	/*std::cout << "Entrer la taille du tableau:\n> ";
 	std::cin >> tablo_gwosè;*/
 	Animal test("Groszizi", 100, 50);
->>>>>>> Stashed changes
 
 }
